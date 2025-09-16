bdffa2d3569af4737ae72ff7dad93c0c8b639f72
Brooke <brooke7827@gmail.com>
2025-09-16 10:11:54 +0930

    Update Caesar cipher, add Vigenere cipher, add .gitignore file

--- codecademy-projects/04-vigenere-cipher.py
+++ codecademy-projects/04-vigenere-cipher.py
@@ -1,18 +1,26 @@
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-def caesar_cypher(original_text, shift_amount, encode_or_decode):
+def vigenere_cipher(original_text, keyword, encode_or_decode):
     cypher_text = ""
+    key_index = 0
 
     for letter in original_text:
         if letter in alphabet:
+            shift_letter = keyword[key_index % len(keyword)]
+
+            if encode_or_decode == "encode":
+                shift_amount = alphabet.index(shift_letter) * -1
+
+            else:
+                shift_amount = alphabet.index(shift_letter)
+
             message_code = alphabet.index(letter) + shift_amount
             cypher_text += alphabet[message_code % len(alphabet)]
 
+            key_index += 1
+
         else:
             cypher_text += letter
-
-    if encode_or_decode == "decode":
-        shift_amount *= -1
 
     print(f"HERE IS THE {encode_or_decode.upper()}D RESULT: {cypher_text}\n")
 
@@ -22,9 +30,9 @@ should_continue = True
 while should_continue == True:
     direction = input("Type 'encode' to encrypt, or type 'decode' to decrypt: ").lower()
     text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number: "))
+    key = input("Type the keyword: ")
 
-    caesar_cypher(text, shift, direction)
+    vigenere_cipher(text, key, direction)
 
     restart = input("Would you like to encode or decode something else? Type 'yes' or 'no': ").lower()
 
